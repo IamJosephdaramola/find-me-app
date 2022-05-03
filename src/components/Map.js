1b8cb8e61b487d894704183595088186330980a6
iamjosephdaramola <daramolaj@gmail.com>
2022-05-03 17:39:34 +0100

    error handled

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,85 +1,87 @@
-import React, { useState, useContext, useEffect, Fragment } from 'react';
-import { GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import React, { useState, useContext, useEffect, Fragment } from "react";
+import { GoogleMap, Marker, InfoWindow } from "react-google-maps";
 // import { FaMapMarker } from 'react-icons/fa';s
-import mapStyles from '../data/mapStyles';
-import Context from '../context/context';
+import mapStyles from "../data/mapStyles";
+import Context from "../context/context";
 
 const Map = () => {
-	const context = useContext(Context);
-	const { latitude, address, getUserAddress, longitude, getLocation } = context;
+  const context = useContext(Context);
+  const { latitude, address, getUserAddress, longitude, getLocation } = context;
 
-	const [lng, setLongitude] = useState(-122.463);
-	const [lat, setLatitude] = useState(37.7648);
-	const [info, setInfo] = useState('San Francisco, US');
-	const [modal, setModal] = useState(false);
+  const [lng, setLongitude] = useState(-122.463);
+  const [lat, setLatitude] = useState(37.7648);
+  const [info, setInfo] = useState("San Francisco, US");
+  const [modal, setModal] = useState(false);
 
-	useEffect(() => {
-		getLocation();
-		getUserAddress();
-		return () => {
-			setLatitude(latitude);
-			setLongitude(longitude);
-			setInfo(address);
-		};
-		// eslint-disable-next-line
-	}, [latitude, longitude]);
+  useEffect(() => {
+    getLocation();
+    getUserAddress();
+    return () => {
+      setLatitude(latitude);
+      setLongitude(longitude);
+      setInfo(address);
+    };
+    // eslint-disable-next-line
+  }, [latitude, longitude]);
 
-	return (
-		<Fragment>
-			<GoogleMap
-				defaultZoom={11}
-				defaultCenter={{ lat: lat, lng: lng }}
-				defaultOptions={{ styles: mapStyles }}>
-				<Marker
-					position={{
-						lat,
-						lng,
-					}}
-					onClick={() => {
-						setModal(true);
-					}}
-					icon={{
-						url: `/skateboarding.svg`,
-						scaledSize: new window.google.maps.Size(50, 50),
-					}}
-				/>
+  return (
+    <Fragment>
+      <GoogleMap
+        defaultZoom={11}
+        defaultCenter={{ lat: lat, lng: lng }}
+        defaultOptions={{ styles: mapStyles }}
+      >
+        <Marker
+          position={{
+            lat,
+            lng,
+          }}
+          onClick={() => {
+            setModal(true);
+          }}
+          icon={{
+            url: `/skateboarding.svg`,
+            scaledSize: new window.google.maps.Size(50, 50),
+          }}
+        />
 
-				{modal && (
-					<InfoWindow
-						position={{
-							lat,
-							lng,
-						}}
-						onCloseClick={() => {
-							setModal(false);
-						}}>
-						<div>
-							<h5>
-								{info === 'San Francisco, US' ||
-								info === '1260 6th Ave, San Francisco, CA 94122, USA'
-									? "user's address is currently unavailable."
-									: info}
-							</h5>
-						</div>
-					</InfoWindow>
-				)}
-			</GoogleMap>
-			<button
-				className='find-me-btn btn btn-warning'
-				type='button'
-				onClick={() => {
-					getLocation();
-					getUserAddress();
-					setLatitude(latitude);
-					setLongitude(longitude);
-					setInfo(address);
-					setModal(true);
-					console.log(latitude, longitude);
-				}}>
-				Find Me
-			</button>
-		</Fragment>
-	);
+        {modal && (
+          <InfoWindow
+            position={{
+              lat,
+              lng,
+            }}
+            onCloseClick={() => {
+              setModal(false);
+            }}
+          >
+            <div>
+              <h5>
+                {info === "San Francisco, US" ||
+                info === "1260 6th Ave, San Francisco, CA 94122, USA"
+                  ? "user's address is currently unavailable."
+                  : info}
+              </h5>
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
+      <button
+        className="find-me-btn btn btn-warning"
+        type="button"
+        onClick={() => {
+          getLocation();
+          getUserAddress();
+          setLatitude(latitude);
+          setLongitude(longitude);
+          setInfo(address);
+          setModal(true);
+        }}
+      >
+        Find Me
+      </button>
+    </Fragment>
+  );
 };
 
 export default Map;
--- src/context/state.js
+++ src/context/state.js
@@ -1,87 +1,91 @@
-import React, { useReducer } from 'react';
-import Context from './context';
-import Reducer from './reducer';
-import { GET_LOCATION, ERROR, GET_ADDRESS } from './types';
+import React, { useReducer } from "react";
+import Context from "./context";
+import Reducer from "./reducer";
+import { GET_LOCATION, ERROR, GET_ADDRESS } from "./types";
 
 const State = (props) => {
-	const initialState = {
-		latitude: 37.7648,
-		longitude: -122.463,
-		address: 'San Francisco, US',
-		error: null,
-	};
-	const [state, dispatch] = useReducer(Reducer, initialState);
+  const initialState = {
+    latitude: 37.7648,
+    longitude: -122.463,
+    address: "San Francisco, US",
+    error: null,
+  };
+  const [state, dispatch] = useReducer(Reducer, initialState);
 
-	const getLocation = () => {
-		try {
-			navigator.geolocation.getCurrentPosition((position) => {
-				dispatch({
-					type: GET_LOCATION,
-					payload: position.coords,
-				});
-			}, handleError);
-		} catch (err) {
-			dispatch({
-				type: ERROR,
-				payload: err.msg,
-			});
-		}
-	};
+  const getLocation = () => {
+    try {
+      navigator.geolocation.getCurrentPosition((position) => {
+        dispatch({
+          type: GET_LOCATION,
+          payload: position.coords,
+        });
+      }, handleError);
+    } catch (err) {
+      dispatch({
+        type: ERROR,
+        payload: err.msg,
+      });
+    }
+  };
 
-	const getUserAddress = async () => {
-		try {
-			const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-			const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${state.latitude},${state.longitude}&sensor=false&key=${process.env.REACT_APP_GOOGLE_API}`;
+  const getUserAddress = async () => {
+    console.log("called");
+    try {
+      //   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+      const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${state.latitude},${state.longitude}&sensor=false&key=${process.env.REACT_APP_GOOGLE_API}`;
 
-			const response = await fetch(proxyUrl + url);
-			const data = await response.json();
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      if (data?.results?.length === 0) return;
 
-			dispatch({
-				type: GET_ADDRESS,
-				payload: data,
-			});
-		} catch (err) {
-			dispatch({
-				type: ERROR,
-				payload: err.msg
-			});
-		}
-	};
+      dispatch({
+        type: GET_ADDRESS,
+        payload: data,
+      });
+    } catch (err) {
+      dispatch({
+        type: ERROR,
+        payload: err.msg,
+      });
+    }
+  };
 
-	const handleError = (error) => {
-		switch (error.code) {
-			case error.PERMISSION_DENIED:
-				alert(
-					'Please grant access to your location, refresh your browser and try again.'
-				);
-				break;
-			case error.POSITION_UNAVAILABLE:
-				alert('Location information is unavailable.');
-				break;
-			case error.TIMEOUT:
-				alert("The request to get user's location timed out.");
-				break;
-			case error.UNKNOWN_ERROR:
-				alert('An unknown error occurred.');
-				break;
-			default:
-				alert('An unknown error occurred');
-		}
-	};
+  const handleError = (error) => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        alert(
+          "Please grant access to your location, refresh your browser and try again."
+        );
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        alert("The request to get user's location timed out.");
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.");
+        break;
+      default:
+        alert("An unknown error occurred");
+    }
+  };
 
-	return (
-		<Context.Provider
-			value={{
-				longitude: state.longitude,
-				latitude: state.latitude,
-				address: state.address,
-				error: state.error,
-				getLocation,
-				getUserAddress,
-			}}>
-			{props.children}
-		</Context.Provider>
-	);
+  return (
+    <Context.Provider
+      value={{
+        longitude: state.longitude,
+        latitude: state.latitude,
+        address: state.address,
+        error: state.error,
+        getLocation,
+        getUserAddress,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
 };
 
 export default State;
